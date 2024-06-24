0ab4ea13b9d669ee5c86d07c0e6e73e0040f2401
AVTPAUL <paul.e@avanticecorp.com>
2024-06-24 12:16:37 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('bm2'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data2.json'
+  })
+
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('bm1'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data.json'
+  })
+
+
+
+// -----------------------------------
+
